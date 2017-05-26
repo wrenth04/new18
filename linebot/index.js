10ea0f41feb15de28a0ea58d9139dc84f81ea34c
wei <wrenth04@gmail.com>
2017-05-26 16:42:08 +0800

    add hot list

--- linebot/index.js
+++ linebot/index.js
@@ -15,6 +15,8 @@ app.post('/webhook', line.middleware(config), (req, res) => {
     .catch(console.log);
 });
 
+app.listen(3000);
+
 const client = new line.Client(config);
 
 function handleEvent(event) {
@@ -23,8 +25,54 @@ function handleEvent(event) {
   }
 
   const roomID = parseInt(event.message.text);
-  if(!roomID) return Promise.resolve(null);
+  const hot = event.message.text.indexOf('hot') != -1;
 
+  if(hot) return getHot(event);
+  if(roomID) return getRoom(event, roomID);
+  return Promise.resolve(null);
+}
+
+function getHot(event) {
+  return axios.post('https://api-dsa.17app.co/api/v1/liveStreams/getSuggestedLiveStreams', {"region":"global","count":10})
+    .then(res => res.data.data)
+    .then(data => JSON.parse(data).sort((a, b) => b.liveViewerCount - a.liveViewerCount))
+    .then(data => {
+      const cards = data.slice(0, 5).map(user => {
+        const {userID, rtmpUrls} = user;
+        const {roomID, name, openID, bio, picture, caption} = user.userInfo;
+        const text = ((bio&&bio.length>50) ? bio.substring(0, 50) + '...' : bio ) || '';
+        return {
+          thumbnailImageUrl: 'https://new18.now.sh/?line=true&roomID='+roomID,
+          title: name,
+          text: text,
+          actions: [
+            {
+              type: 'uri',
+              label: '直播 ' + roomID,
+              uri: rtmpUrls[0].url
+            },
+            {
+              type: 'uri',
+              label: '資訊',
+              uri: 'http://17.media/share/user/'+openID
+            }
+          ]
+        };
+      });
+      const msg = {
+        type: 'carousel',
+        columns: cards
+      };
+
+      return client.replyMessage(event.replyToken, {
+        type: 'template',
+        altText: 'hot list',
+        template: msg
+      });
+    });
+}
+
+function getRoom(event, roomID) {
   return axios.get('http://17.media/share/live/'+roomID)
     .then(res => res.data)
     .then(html => JSON.parse(html.split('appData = ')[1].split('</script')[0]))
@@ -33,7 +81,7 @@ function handleEvent(event) {
       const {userInfo, liveStatus} = json.liveStream;
       const {name, openID, bio, picture, caption} = userInfo;
 
-      const text = ((bio&&bio.length>200) ? bio.substring(0, 200) : bio ) || '';
+      const text = ((bio&&bio.length>50) ? bio.substring(0, 50)+'...' : bio ) || '';
 
       const msg = {
         type: 'template',
@@ -46,7 +94,7 @@ function handleEvent(event) {
           actions: [
             {
               type: 'uri',
-              label: '直播',
+              label: '直播 ' + roomID,
               uri: url
             },
             {
@@ -61,6 +109,3 @@ function handleEvent(event) {
       return client.replyMessage(event.replyToken, msg);
     });
 }
-
-app.listen(3000);
-
